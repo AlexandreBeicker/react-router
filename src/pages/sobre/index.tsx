67068293561338814068938a35d690efa73b0100
Alexandre Marquez de Moura Beicker <alexandremmbeicker@gmail.com>
2023-10-11 14:11:48 -0300

    criacao de paginas com as rotas

---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export function Sobre () {
+    return(
+        <div>
+            <h1>Bem vindo a página sobre</h1>
+            <Link to="/">Voltar a página home</Link>
+        </div>
+    )
+}
